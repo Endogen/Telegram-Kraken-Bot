03f516bde50fbfa5a05581063ca9fc38306e674c
Endogen <081081@gmx.net>
2017-08-20 23:56:00 +0200

    * Commented out 'updater.idle()' to see if notifications get triggered

--- telegram_kraken_bot.py
+++ telegram_kraken_bot.py
@@ -1020,4 +1020,4 @@ monitor_open_orders()
 # Run the bot until you press Ctrl-C or the process receives SIGINT,
 # SIGTERM or SIGABRT. This should be used most of the time, since
 # start_polling() is non-blocking and will stop the bot gracefully.
-updater.idle()
+# updater.idle()
